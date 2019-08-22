b599c4aaaccb3d1fa9eddae9dea6240e0a4951f3
liciolentimo <lentimo@gmail.com>
2019-08-22 15:44:55 +0300

    Initial commit

---
+++ app/src/main/java/com/example/paypalandroid/Config/Config.java
@@ -0,0 +1,5 @@
+package com.example.paypalandroid.Config;
+
+public class Config {
+    public static final String PAYPAL_CLIENT_ID = "AW3FyAU_FbMwxVcu4wDdQcpUYEfxgwiRbZezuvccQjyHLm389OZvDTOGte10nSVmbXlpu8fxxWFiDqH4";
+}
---
+++ app/src/main/java/com/example/paypalandroid/PaymentDetails.java
@@ -0,0 +1,43 @@
+package com.example.paypalandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class PaymentDetails extends AppCompatActivity {
+    TextView txtId,txtAmount,txtStatus;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_details);
+
+        txtId = findViewById(R.id.txtId);
+        txtAmount = findViewById(R.id.txtAmount);
+        txtStatus = findViewById(R.id.txtStatus);
+
+        Intent intent = getIntent();
+
+        try {
+            JSONObject jsonObject = new JSONObject(intent.getStringExtra("PaymentDetails"));
+            showDetails(jsonObject.getJSONObject("response"),intent.getStringExtra("PaymentAmount"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showDetails(JSONObject response, String paymentAmount) {
+        try {
+            txtId.setText(response.getString("id"));
+            txtStatus.setText(response.getString("state"));
+            txtAmount.setText("$"+paymentAmount);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
